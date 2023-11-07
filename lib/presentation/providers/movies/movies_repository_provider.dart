1c5e958e58a191c13a67cc86b2a3a0ea4e022f42
Luis Berrezueta <laberreueta2@protonmail.com>
2023-11-06 21:22:25 -0500

    show titles

--- lib/presentation/providers/movies/movies_repository_provider.dart
+++ lib/presentation/providers/movies/movies_repository_provider.dart
@@ -3,6 +3,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:cinemapedia/infrastructure/repositories/movie_repository_impl.dart';
 import 'package:cinemapedia/infrastructure/dataSources/moviedb_data_source_impl.dart';
 
-final MovieRepositoryProvider = Provider((ref) {
+final movieRepositoryProvider = Provider((ref) {
   return MovieRepositoryImpl(dataSource: MovieDbDataSourceImpl());
 });
