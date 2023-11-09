bc8e79322e700743d1ad768ee618e858e75a4b21
Luis Berrezueta <laberreueta2@protonmail.com>
2023-11-08 23:16:18 -0500

    swiper

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:cinemapedia/presentation/providers/movies/movies_providers.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -38,14 +39,11 @@ class _HomeViewState extends ConsumerState<_HomeView> {
       );
     }
 
-    return ListView.builder(
-      itemCount: nowPlayingMovies.length,
-      itemBuilder: (context, index) {
-        final movie = nowPlayingMovies[index];
-        return ListTile(
-          title: Text(movie.title),
-        );
-      },
+    return Column(
+      children: [
+        const CustomAppBar(),
+        MoviesSlidesShow(movies: nowPlayingMovies)
+      ],
     );
   }
 }
---
+++ lib/presentation/widgets/widgets.dart
@@ -0,0 +1,3 @@
+export 'package:cinemapedia/presentation/widgets/movies/movies_slides_show.dart';
+
+export 'package:cinemapedia/presentation/widgets/shared/custom_app_bar.dart';
