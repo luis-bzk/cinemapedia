61254767bd42a7f71831af623949498f480d09fc
Luis Berrezueta <laberreueta2@protonmail.com>
2023-11-20 11:57:44 -0500

    Navegar a otra pantalla con parámetros

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -5,14 +5,16 @@ final appRouter = GoRouter(initialLocation: '/', routes: [
   GoRoute(
       path: '/',
       name: HomeScreen.name,
-      builder: (context, state) => const HomeScreen()),
-  GoRoute(
-      path: '/movie/:id',
-      name: MovieScreen.name,
-      builder: (context, state) {
-        final movieID = state.pathParameters['id'] ?? 'no-id';
-        return MovieScreen(
-          movieId: movieID,
-        );
-      })
+      builder: (context, state) => const HomeScreen(),
+      routes: [
+        GoRoute(
+            path: 'movie/:id',
+            name: MovieScreen.name,
+            builder: (context, state) {
+              final movieID = state.pathParameters['id'] ?? 'no-id';
+              return MovieScreen(
+                movieId: movieID,
+              );
+            })
+      ]),
 ]);
